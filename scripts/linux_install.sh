a5ecd2d3899ec189231891e8ed7fc778cceefd2e
martin legrand <martin.legrand@epitech.eu>
2025-03-30 16:13:44 +0200

    fix : improve install script

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -2,23 +2,34 @@
 
 echo "Starting installation for Linux..."
 
+set -e
 # Update package list
-sudo apt-get update
+sudo apt-get update || { echo "Failed to update package list"; exit 1; }
 # make sure essential tool are installed
-sudo apt install python3-dev python3-pip python3-wheel build-essential alsa-utils
+# Install essential tools
+sudo apt-get install -y \
+    python3-dev \
+    python3-pip \
+    python3-wheel \
+    build-essential \
+    alsa-utils \
+    portaudio19-dev \
+    python3-pyaudio \
+    libgtk-3-dev \
+    libnotify-dev \
+    libgconf-2-4 \
+    libnss3 \
+    libxss1 || { echo "Failed to install packages"; exit 1; }
+
 # upgrade pip
 pip install --upgrade pip
-# install port audio
-sudo apt-get install portaudio19-dev python-pyaudio python3-pyaudio
-# install chromedriver misc
-sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1
 # install wheel
 pip install --upgrade pip setuptools wheel
 # install docker compose
-sudo apt install docker-compose
-# Install Python dependencies from requirements.txt
-pip3 install --no-cache-dir --force-reinstall -r requirements.txt
+sudo apt install -y docker-compose
 # Install Selenium for chromedriver
 pip3 install selenium
+# Install Python dependencies from requirements.txt
+pip3 install -r requirements.txt
 
 echo "Installation complete for Linux!"
